545ef68573979fc3720120949087707c949b2d62
luckxudong <LuckXudong@gmail.com>
2012-02-14 01:32:33 +0000

    简化engine  game  game view

--- src/com/jason/Demo.java
+++ src/com/jason/Demo.java
@@ -1,7 +1,5 @@
 package com.jason;
 
-import android.os.Bundle;
-import android.view.Display;
 import com.jason.graphic.DemoView;
 import com.jason.scene_providers.Main;
 import ice.engine.Game;
@@ -9,18 +7,6 @@ import ice.engine.GameView;
 import ice.engine.SceneProvider;
 
 public class Demo extends Game {
-    public static final float Z_NEAR = 0.1f, Z_FAR = 1000;
-
-    //Z_NEAR = 0.01f居然会出问题而0.1f不会？
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        Display display = getWindowManager().getDefaultDisplay();
-        width = display.getWidth();
-        height = display.getHeight();
-
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     protected Class<? extends SceneProvider> getEntry() {
@@ -32,15 +18,4 @@ public class Demo extends Game {
         return new DemoView(this);
     }
 
-    @Override
-    public int getWidth() {
-        return width;
-    }
-
-    @Override
-    public int getHeight() {
-        return height;
-    }
-
-    private int width, height;
 }
